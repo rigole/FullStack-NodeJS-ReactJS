b1e10eb5a677d17ef53a75fbedafc2740a265df4
rigole <foplacide@gmail.com>
2022-06-06 21:27:31 +0100

    adding passport and google auth passport and setting passport

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-auth').Strategy
 const app = express();
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.send({hi: 'Hello world'});
-})
+})*/
+
+passport.use(new GoogleStrategy())
 
 const PORT = process.env.PORT || 3000;
 
